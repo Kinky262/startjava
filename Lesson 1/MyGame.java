d43a8a6201e72146437ce9f537d9689ed47c49f3
Natalia Pyryaeva <likemartini2@gmail.com>
2018-09-20 01:11:44 +0300

    Variables were renamed. The loop is excluded.

--- Lesson 1/MyGame.java
+++ Lesson 1/MyGame.java
@@ -1,17 +1,14 @@
 public class MyGame {
 	public static void main (String[] args) {
-		int goalNum = 42; // число, "загаданное" компьютером
-		int userTry = 15; // число, вводимое игроком
+		int computerNumber = 42; 
+		int userNumber = 27; 
 
-		for (boolean isWin = false; !isWin; ) {
-			if (goalNum == userTry) {
-				isWin = true;
-				System.out.println("You won! --> isWin = " + isWin);
-			} else if (goalNum > userTry) {
-				System.out.println("The number you entered is less than what the computer");
-			} else if (goalNum < userTry) {
-				System.out.println("The number you entered is greater than what the computer");
-			} 
-		}
+		if (computerNumber == userNumber) {
+			System.out.println("You won!");
+		} else if (computerNumber > userNumber) {
+			System.out.println("The number you entered is less than what the computer");
+		} else {
+			System.out.println("The number you entered is greater than what the computer");
+		} 		
 	}
 }
