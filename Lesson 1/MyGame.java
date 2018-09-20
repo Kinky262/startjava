eb3866af7a4f14dcc59a81cae104e56b2b5dc9b2
Natalia Pyryaeva <likemartini2@gmail.com>
2018-09-20 11:54:18 +0300

    While-loop was added.

--- Lesson 1/MyGame.java
+++ Lesson 1/MyGame.java
@@ -1,14 +1,18 @@
 public class MyGame {
 	public static void main (String[] args) {
 		int computerNumber = 42; 
-		int userNumber = 27; 
+		int userNumber = 45; 
+		boolean isWin = false;
 
-		if (computerNumber == userNumber) {
-			System.out.println("You won!");
-		} else if (computerNumber > userNumber) {
-			System.out.println("The number you entered is less than what the computer");
-		} else {
-			System.out.println("The number you entered is greater than what the computer");
+		while (!isWin) {
+			if (computerNumber == userNumber) {
+				System.out.println("You won!");
+				isWin = true;
+			} else if (computerNumber > userNumber) {
+				System.out.println("The number you entered is less than what the computer");
+			} else {
+				System.out.println("The number you entered is greater than what the computer");
+			}
 		} 		
 	}
 }
