787a93af8066ee0241a1fb0c85ae5dda68e0889b
Natalia Pyryaeva <likemartini2@gmail.com>
2018-09-20 21:22:47 +0300

    1. While-condition is changed in better way. 2. User actions emulation was added.

--- Lesson 1/MyGame.java
+++ Lesson 1/MyGame.java
@@ -1,17 +1,40 @@
 public class MyGame {
 	public static void main (String[] args) {
 		int computerNumber = 42; 
-		int userNumber = 45; 
+		int userNumber = 18; 
 		boolean isWin = false;
+		int lowerUserNum = 0;
+		int upperUserNum = 100;
 
-		while (!isWin) {
+		while (true) {
+			System.out.println("You entered: " + userNumber);
+
 			if (computerNumber == userNumber) {
-				System.out.println("You won!");
+				System.out.println("You won!\n");
 				isWin = true;
+				break;
 			} else if (computerNumber > userNumber) {
-				System.out.println("The number you entered is less than what the computer");
+				lowerUserNum = userNumber;
+				System.out.println("The number you entered is LESS than what the computer\n");
+
+				if ((userNumber + 14) <= 100 && (userNumber + 14) <= upperUserNum) {
+					userNumber = userNumber + 14;					
+				} else if ((userNumber + 9) <= 100 && (userNumber + 9) <= upperUserNum) {
+					userNumber = userNumber + 9;		
+				} else {
+					userNumber++;
+				}
 			} else {
-				System.out.println("The number you entered is greater than what the computer");
+				upperUserNum = userNumber;				
+				System.out.println("The number you entered is GREATER than what the computer\n");
+
+				if ((userNumber - 15) >= 0 && (userNumber - 15) >= lowerUserNum) {
+					userNumber = userNumber - 15;					
+				} else if ((userNumber - 10) >= 0 && (userNumber - 10) >= lowerUserNum) {
+					userNumber = userNumber - 10;					
+				} else {
+					userNumber--;
+				}
 			}
 		} 		
 	}
