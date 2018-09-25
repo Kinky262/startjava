2dd80438b316ff35cac6c1c4360df0230fb8f1cb
Natalia Pyryaeva <likemartini2@gmail.com>
2018-09-25 17:38:51 +0300

    Input process is implemented with Scanner

--- Lesson 2/Calculator.java
+++ Lesson 2/Calculator.java
@@ -1,48 +1,45 @@
 public class Calculator {
 
-	int  num1 = 2;
-	int  num2 = 3;
-	char mathOperation = '^'; // присваеваем переменной значение математической операции
+	int  num1;
+	int  num2;
+	String mathOperation;
 	int  result = 0;
-	boolean isError = false;
 
-public void inUse()	{
-	switch(mathOperation) {
-		case '+':	result = num1 + num2;
-					break;
-		case '-':	result = num1 - num2;
-					break;
-		case '*':	result = num1 * num2;
-					break;
-		case '/':	if (num2 == 0) {
-						System.out.println("Division by zero is not allowed!"); 
-					} else {
-						result = num1 / num2;
-					}
-					break;
-		case '^':	result = num1;
-
-					if (num1 != 0 && num2 == 0) {
-						result = 1;
-					} else if (num1 == 0) {
-						System.out.println("Zero can not be raised to a power...");
-					} else {
-						for (int i = 1; i < num2; i++) {  
-							result *= num1;
+	public void inUse()	{
+		switch(mathOperation) {
+			case "+":	result = num1 + num2;
+						break;
+			case "-":	result = num1 - num2;
+						break;
+			case "*":	result = num1 * num2;
+						break;
+			case "/":	if (num2 == 0) {
+							System.out.println("Division by zero is not allowed!"); 
+						} else {
+							result = num1 / num2;
 						}
-	    			}
-	    			break;
-		case '%':	result = num1 % num2;
-					break;
-		default:	System.out.println("Wrong operation type! try again...");
-					isError = true;
-					break;
-	}
+						break;
+			case "^":	result = num1;
 
-	if (isError || (mathOperation == '/' && num2 == 0) || (mathOperation == '^' && num1 == 0)) {
-		System.out.println("No result...");
-	} else {
-		System.out.println(num1 + " " + mathOperation + " " + num2 + " = " + result);
-	}	
-}		
+						if (num1 != 0 && num2 == 0) {
+							result = 1;
+						} else if (num1 == 0) {
+							System.out.println("Zero can not be raised to a power...");
+						} else {
+							for (int i = 1; i < num2; i++) {  
+								result *= num1;
+							}
+	    				}
+	   		 			break;
+			case "%":	result = num1 % num2;
+						break;
+			default:	break;						
+		}
+
+		if ((mathOperation == "/" && num2 == 0) || (mathOperation == "^" && num1 == 0)) {
+			System.out.println("No result..."); 
+		} else {
+			System.out.println(num1 + " " + mathOperation + " " + num2 + " = " + result);
+		}	
+	}		
 }
--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -1,7 +1,15 @@
+import java.util.Scanner; 
+
 public class CalculatorTest {
 	public static void main (String[] args) {
-	
-	Calculator test = new Calculator();
-	test.inUse();
+		Calculator calculate = new Calculator();
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a first number: ");
+		calculate.num1 = scan.nextInt();
+		System.out.print("Enter a sign of math operation: ");
+		calculate.mathOperation = scan.next();
+		System.out.print("Enter a second number: ");
+		calculate.num2 = scan.nextInt();
+		calculate.inUse();
 	}
 }
