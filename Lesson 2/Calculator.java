39348fef055d956824d5f2fdc063b524a8aa71e1
Natalia Pyryaeva <likemartini2@gmail.com>
2018-09-30 23:44:04 +0300

    notes processed

--- Lesson 2/Calculator.java
+++ Lesson 2/Calculator.java
@@ -1,46 +1,47 @@
 public class Calculator {
 
-	int num1;
-	int num2;
-	String mathOperation;
+	protected int num1;
+	protected int num2;
+	protected String mathOperation;
 
 	public void mathOperations()	{
 		int result = 0;
+		
 		switch(mathOperation) {
 			case "+":	
-			 result = num1 + num2;
-			 break;
+				result = num1 + num2;
+			    break;
 			case "-":	
-			 result = num1 - num2;
-			 break;
+				result = num1 - num2;
+				break;
 			case "*":	
-			 result = num1 * num2;
-			 break;
+				result = num1 * num2;
+				break;
 			case "/":	
-			 if (num2 == 0) {
-				System.out.println("Division by zero is not allowed!"); 
-			 } else {
-				result = num1 / num2;
-			 }
-			 break;
+				if (num2 == 0) {
+					System.out.println("Division by zero is not allowed!"); 
+			 	} else {
+					result = num1 / num2;
+			 	}
+				break;
 			case "^":	
-			 result = num1;
+				result = num1;
 
-			 if (num1 != 0 && num2 == 0) {
-				result = 1;
-			 } else if (num1 == 0) {
-				System.out.println("Zero can not be raised to a power...");
-			 } else {
-				for (int i = 1; i < num2; i++) {  
-					result *= num1;
-				}
-	    	 }
-	   		 break;
+				if (num1 != 0 && num2 == 0) {
+					result = 1;
+				} else if (num1 == 0) {
+					System.out.println("Zero can not be raised to a power...");
+				} else {
+					for (int i = 1; i < num2; i++) {  
+						result *= num1;
+					}
+	    		}
+	   			break;
 			case "%":	
-			 result = num1 % num2;
-			 break;
+				result = num1 % num2;
+				break;
 			default:	
-			 break;						
+				break;						
 		}
 
 		if ((mathOperation.equals("/") && num2 == 0) || (mathOperation.equals("^") && num1 == 0)) {
--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class CalculatorTest {
 	public static void main (String[] args) {
-		String userAnswer = null;
-		boolean isContinue = true;
-		boolean isAnswer = false;
-		while (isContinue == true) {
-			Calculator calculate = new Calculator();
-			Scanner scan = new Scanner(System.in);
+		String userAnswer = "y";		
+		Calculator calculate = new Calculator();
+		Scanner scan = new Scanner(System.in);
+
+		do {
 			System.out.print("Enter a first number: ");
 			calculate.num1 = scan.nextInt();
 			System.out.print("Enter a sign of math operation: ");
@@ -15,23 +14,16 @@ public class CalculatorTest {
 			System.out.print("Enter a second number: ");
 			calculate.num2 = scan.nextInt();
 			calculate.mathOperations();
-			isAnswer = false;
-			while (isAnswer == false) {
-				System.out.print("Would you like to continue? [Y/N]: ");
-				userAnswer = scan.next();							
-				if (userAnswer.equals("Y") || userAnswer.equals("y")|| userAnswer.equals("N") || userAnswer.equals("n")) {
-					isAnswer = true;
-					break;
-				} else {
-					isAnswer = false;
-				}		 
-			}
 
-			if (userAnswer.equals("Y") || userAnswer.equals("y")) {
-				isContinue = true;
-			} else {
-				isContinue = false; 
-			}
-		}	
+			do {
+				System.out.print("Would you like to continue? [Y/N]: ");
+				userAnswer = scan.next();	
+
+				if (userAnswer.equals("Y") || userAnswer.equals("y")|| userAnswer.equals("N") || userAnswer.equals("n")) {
+					break;
+				}
+
+			} while (!userAnswer.equals("Y") || !userAnswer.equals("y")|| !userAnswer.equals("N") || !userAnswer.equals("n"));
+		} while (userAnswer.equals("Y") || userAnswer.equals("y"));
 	}
 }
