1c90965d21b3f4139bfe6f30197c17a28cf7a10a
Natalia Pyryaeva <likemartini2@gmail.com>
2018-10-11 16:14:03 +0300

    final refactoring.

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -16,7 +16,8 @@ public class CalculatorTest {
 			do {
 				System.out.print("Would you like to continue? [Y/N]: ");
 				userAnswer = scan.nextLine();
-			} while (!userAnswer.equals("Y") && !userAnswer.equals("y") && !userAnswer.equals("N") && !userAnswer.equals("n"));
+			} while (!userAnswer.equals("Y") && !userAnswer.equals("y") &&
+                    !userAnswer.equals("N") && !userAnswer.equals("n"));
 		} while (userAnswer.equals("Y") || userAnswer.equals("y"));
 	}
 
